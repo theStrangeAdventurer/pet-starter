9b165e77c02d202fc4019b2210e6142f7821174c
Алексей Зайцев <aleksejzajcev@mbpro-16.local>
2021-09-30 15:52:40 +0300

    added static serve from nginx

--- client/src/server/index.tsx
+++ client/src/server/index.tsx
@@ -12,13 +12,11 @@ import { prepareAssetsHtml } from './utils/prepare-assets-html';
 import { Helmet } from 'react-helmet';
 
 const app = express();
-const assetsFolder = path.resolve(__dirname, 'public');
 const assetsManifest = path.resolve(__dirname, 'public', 'manifest.json');
 const manifest: Buffer = fs.readFileSync(assetsManifest);
 const parsedManifest = JSON.parse(manifest.toString());
 const { jsScriptsHtml, cssLinkshtml } = prepareAssetsHtml(parsedManifest);
 
-app.use('/public', express.static(assetsFolder));
 app.set('view engine', 'ejs');
 app.engine('ejs', ejs);
 app.set('views', path.join(__dirname, 'views'));
